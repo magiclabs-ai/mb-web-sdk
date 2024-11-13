8d7085b51105593f437907084835222f014104d7
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2024-11-13 10:13:49 +0000

    wip

--- packages/web-sdk/tsup.config.ts
+++ packages/web-sdk/tsup.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
   format: ["esm", "cjs"],
   sourcemap: true,
   env: {
-    API_HOST: process.env.API_HOST || "",
-    WS_RECONNECT_INTERVAL: process.env.WS_RECONNECT_INTERVAL || "",
+    API_HOST: process.env.API_HOST!,
+    WS_RECONNECT_INTERVAL: process.env.WS_RECONNECT_INTERVAL!,
   },
   target: "esnext",
   outDir: "dist",
