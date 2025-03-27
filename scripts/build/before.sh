64775d3a1733aa268672b005424117364efd5460
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2025-03-27 20:12:33 +0000

    [wip] Remove before build

--- scripts/build/before.sh
+++ scripts/build/before.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-rm -rf ./dist
+# rm -rf ./dist
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-cd $SCRIPT_DIR
+# SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+# cd $SCRIPT_DIR
 
-if [[ "$1" == "--watch" ]]; then
-  node ../create-tsconfig.js --dev
-else
-  node ../create-tsconfig.js
-fi
+# if [[ "$1" == "--watch" ]]; then
+#   node ../create-tsconfig.js --dev
+# else
+#   node ../create-tsconfig.js
+# fi
